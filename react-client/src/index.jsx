52ee76444aeb2fd1fd6897a78c716ceb9d3dffe2
Seif-Miehiar <Saif.muhiar97@gmail.com>
2019-05-19 01:49:42 +0300

    signing-class

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,42 +2,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import { Button, Modal, OverlayTrigger, Popover, Tooltip, Nav, NavItem, Row, Col, FormGroup, FieldGroup, Checkbox,Form } from 'react-bootstrap'
-
+import SignIn from './auth/SignIn.jsx';
 import SignUp from './auth/SignUp.jsx';
-
-
-
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-    
+    this.state = {
+      repos: []
     }
-
-
+    // console.log(this.props.repos)
   }
-
-  updateState(data){
+  updateState(data) {
     this.setState({
     })
   }
-  
 
-  render () {
+  render() {
     return (<div>
-
-      <h1>rawan</h1>
-
-      <h1>Item List</h1>
-      <SignUp repos = {this.state.repos} />
-      
-
-     
+      <h1>SignIn</h1>
+      <SignIn />
     </div>)
   }
 }
 
 
- 
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
