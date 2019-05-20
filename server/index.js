229da4d1c59966629d83cbf977c91f7a50f502c8
RawanAkAzz <rawan.akram27@yahoo.com>
2019-05-20 14:52:06 +0300

    update-signup-Auth

--- server/index.js
+++ server/index.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-// const bcrypt = require('bcrypt');
-//const jwt = require('jsonwebtoken');
+const { userName } = require('./database-mongo/dbmongo');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
-
-
-//const items = require('../database-mongo');
-
 
 
 const app = express();
@@ -35,7 +32,7 @@ app.get("/", (req, res) => {
   res.send(" This is working !")
 })
 
-app.post ('/signinUser', (req,res)=> {
+app.post('/signinUser', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   console.log("ok")
@@ -48,25 +45,41 @@ app.post('/signUp', (req, res) => {
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const password = req.body.password;
-  const passowrdHash = bcrypt.hashSync(password , 8);
-  userName.create({firstName : " " , lastName : " " , password : passowrdHash}).then(function(){
-     return res.status(HTTP_Created).send("Sign up successful");
-  }).catch(function(err){
-     if(err.code === 11000) {
-        return res.status(HTTP_BAD_REQUEST).send("You can't use this username")
-     }
-     return res.status(HTTP_SERVER_ERROR).send('Server Error');
+  const passowrdHash = bcrypt.hashSync(password, 8);
+  userName.create({ username: { firstName: firstName, lastName: lastName }, password: passowrdHash }).then(function () {
+    return res.status(HTTP_Created).send("Sign up successful");
+  }).catch(function (err) {
+    if (err.code === 11000) {
+      return res.status(HTTP_BAD_REQUEST).send("You can't use this username")
+    }
+    return res.status(HTTP_SERVER_ERROR).send('Server Error');
   });
   console.log("ok")
   res.send("Done")
 });
 
-app.post('/signinClient',(req, res) => {
-   const email = req.body.email;
-   const password = req.body.password
-})
+app.post('/signinClient', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  userName.findOne({ firstName: firstName }).then(function (user) {
+    if (!userName) {
+      return res.status(HttP_AUNAUTHORIZED).send({ error: "You Can Sign Up Here" });
+    }
+    const existPass = userName.password;
+    bcrypt.compare(password, existPass).then(function (Matching) {
+      if (Matching) {
+        const token = jwt.sign({ username: user.username }, SECRET_KEY, { expiresIn: 4000 });
+        return res.send({ token: token });
+      } else {
+        return res.status(HTTP_UNAUTHORIZED).send({ error: 'Wrong password' });
+      }
+    });
+  });
+  
+});
+    
 
 app.listen(port, function () {
-  console.log(`listening on port ${port}`);
-});
+      console.log(`listening on port ${port}`);
+    });
 
