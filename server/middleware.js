34271e99f5de17c73fd18d11873c004e1015aac2
motasem6236 <motasem6236@gmail.com>
2019-05-20 15:35:38 +0300

    motasemfayez

--- server/middleware.js
+++ server/middleware.js
@@ -4,7 +4,7 @@ const { HTTP_UNAUTHORIZED, HTTP_SERVER_ERROR } = require("./error_type.js");
 const { SECRET_KEY } = require("./secret.js");
 const { userName } = require("database-mongo/dbmongo.js");
 
-const bodyParser = express.json();
+
 
 const authenticate = function(req, res, next){
     const token = req.headers['x-access-token']; //Username encoded in token
@@ -12,18 +12,18 @@ const authenticate = function(req, res, next){
         return res.status(HTTP_UNAUTHORIZED).send('Please sign in');
     }
     jwt.verify(token, SECRET_KEY, function(err, decodedToken){
-        //If err, token invalid
+       
         if(err){
           return res.status(HTTP_UNAUTHORIZED).send('Please sign in');
        }
-        //Check if user exists in the database
+       
         const username = decodedToken.username;
         User.findOne({username: username}).then(function(user){
             console.log(user);
             if(!user){
                 return res.status(HTTP_UNAUTHORIZED).send('Please sign up');
             }
-            req.body.user = user; //Put user in req.body
+            req.body.user = user; 
             return next();
         }).catch(function(err){
             return res.status(HTTP_SERVER_ERROR).send(err);
@@ -32,5 +32,5 @@ const authenticate = function(req, res, next){
 };
 
 
-exports.bodyParser = bodyParser;
+
 exports.authenticate = authenticate;
